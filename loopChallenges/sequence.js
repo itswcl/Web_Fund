1fa7fe4e8c371cd0cbdd1937a11d53077858a7dd
weicheng lee <z82111712@gmail.com>
2021-10-14 13:43:02 -0700

    print sequence with decrement

--- loopChallenges/sequence.js
+++ loopChallenges/sequence.js
@@ -1,4 +1,13 @@
 /*
 3 Print the sequence
-Using a loop write code that will console.log the values in this sequence 4, 2.5, 1, -0.5, -2, -3.5.
+Using a loop write code that will console.log the values in this sequence 4, 2.5, 1, -0.5, -2, -3.5. // -1.5
 */
+
+function printSeq(start, end) {
+    // iterate from start to end number with decrement 1.5
+    for (var i = start; i >= end; i-=1.5) {
+        // print out each number 
+        console.log(i)
+    };
+}
+printSeq(4, -3.5)
