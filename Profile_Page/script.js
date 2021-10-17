aad8ba0e7f522aa048e052f049ff8cb551766178
weicheng lee <z82111712@gmail.com>
2021-10-17 13:41:10 -0700

    update connection increase with ++ sign to increase by 1

--- Profile_Page/script.js
+++ Profile_Page/script.js
@@ -17,18 +17,24 @@ function updateName() {
 // get all the request card
 var requests = document.querySelectorAll('.con-row-request');
 // turn the connection count to integer
-var connectionCon = parseInt(document.querySelector('.count-connection').innerText);
 
 function removeEle(index) {
     // remove the specific index of tag
     requests[index].remove();
-    // decrease the request count
-    document.querySelector('.count_active').innerText -= 1;
+    // decrease the request count using -- by 1
+    document.querySelector('.count_active').innerText--;
 }
 
+// var connectionCon = parseInt(document.querySelector('.count-connection').innerText);
+// function connectCounts() {
+//     // add 1 if accept click
+//     connectionCon += 1;
+//     // update new connection count
+//     document.querySelector('.count-connection').innerText = connectionCon;
+// }
+
+
 function connectCounts() {
-    // add 1 if accept click
-    connectionCon += 1;
-    // update new connection count
-    document.querySelector('.count-connection').innerText = connectionCon;
+    // update the connection count with ++ to increase by 1
+    document.querySelector('.count-connection').innerText++;
 }
