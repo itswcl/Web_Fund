f1d0a5e68e8226156e618d02a381ad69a6e772e5
weicheng lee <z82111712@gmail.com>
2021-10-17 20:45:15 -0700

    update tempSwitch

--- DojoWeather/script.js
+++ DojoWeather/script.js
@@ -10,17 +10,16 @@ function hide() {
     document.querySelector("#cookie").remove();
 }
 
-
-
 function tempSwitch(e) {
     if (e.value === "F") {
-        // (c * 9 / 5) + 32;
         for (var i = 0; i < tempData.length; i++) {
+            // (c * 9 / 5) + 32;
             var temp = parseInt(tempData[i].innerText);
             tempData[i].innerText = ((temp * 9 / 5) + 32).toFixed(0) + "°";
         }
     } else if (e.value === "C") {
         for (var i = 0; i < tempData.length; i++) {
+            // (f - 32) * 5 / 9
             var temp = parseInt(tempData[i].innerText);
             tempData[i].innerText = ((temp - 32) * 5 / 9).toFixed(0) + "°";
         }
