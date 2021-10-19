8ab1a676d3509a882d05a9d05e9eee657a56ee4a
weicheng lee <z82111712@gmail.com>
2021-10-19 14:43:49 -0700

    update with multiple tempting enter numbers

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,49 +1,48 @@
 var result = [];
-var tempResult;
-var signs = ["+", "-", "/", "*"]
+var tempRes = [];
+// var signs = ["+", "-", "/", "*"]
 
 function press(n) {
-    if (n > 0) {
-        result.push(n);
-    } else if (n === 0 && result[0] !== undefined) {
-        result.push(n);
-    } else if (n === '.' && result[0] !== undefined && result.indexOf('.') === -1) {
-        result.push(n);
-    } 
+        if (n > 0) {
+            result.push(n);
+        } else if (n === 0 && result[0] !== undefined) {
+            result.push(n);
+        } else if (n === '.' && result[0] !== undefined && result.indexOf('.') === -1) {
+            result.push(n);
+        } 
+    
+        if (result.length === 0) {
+            document.querySelector("#display").innerText = 0;
+        // check if there's sign in the array
+        // include function 
+        // signs.indexOf(result[result.length-2]) !== -1
+        } else if (result.includes("+") || result.includes("-") || result.includes("*") || result.includes("/")) {
+            tempRes[tempRes.length - 1].push(n);
+            document.querySelector("#display").innerText = tempRes[tempRes.length - 1].join("");
+        }   else {
+            document.querySelector("#display").innerText = result.join("");
+        } 
+        console.log(result);
+        console.log(tempRes);
 
-    if (result.length === 0) {
-        document.querySelector("#display").innerText = 0;
-        console.log("if")
-    // check if there's sign in the array
-    // include function 
-    } else if (signs.indexOf(result[result.length-2]) !== -1) {
-        // tempRes.push(n);
-        document.querySelector("#display").innerText = n;
-        // console.log(tempRes);
-        console.log("else if")
-    }   else {
-        document.querySelector("#display").innerText = result.join("");
-        console.log("else")
-    } 
-    console.log(result);
 }
 
 function setOP(sign) {
     if (typeof result[result.length - 1] === 'number') {
         result.push(sign);
     }
-    // calculate();
+    tempRes.push([]);
 }
 
 function calculate() {
     var finalAns = eval(result.join(''));
     document.querySelector("#display").innerText = finalAns;
-    tempResult = finalAns;
-    result = [tempResult];
+    result = finalAns;
+    tempRes = [];
 }
 
 function clr() {
     result = [];
-    tempResult = 0;
+    tempRes = [];
     document.querySelector("#display").innerText = 0;
 }
